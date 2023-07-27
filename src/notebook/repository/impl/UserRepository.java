3f7d0fe50ac3a4867598388578c1e48801772d29
Tatiana <t-b-k@bk.ru>
2023-07-27 10:11:07 +0300

    Moved readAll() and saveAll() to class UserRepository 10:10 27-07-2023

--- src/notebook/repository/impl/UserRepository.java
+++ src/notebook/repository/impl/UserRepository.java
@@ -5,52 +5,96 @@ import notebook.mapper.impl.UserMapper;
 import notebook.model.User;
 import notebook.repository.GBRepository;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepository implements GBRepository<User, Long> {
     private final UserMapper mapper;
-    private final FileOperation operation;
+    private final String fileName;
+//    private final FileOperation operation;
 
-    public UserRepository(FileOperation operation) {
+    public UserRepository(String fileName) {
         this.mapper = new UserMapper();
-        this.operation = operation;
+        this.fileName = fileName;
     }
 
     @Override
     public List<User> findAll() {
-        List<String> lines = operation.readAll();
+        List<String> lines = readAll();
         List<User> users = new ArrayList<>();
         for (String line : lines) {
             users.add(mapper.toOutput(line));
         }
         return users;
     }
+    private List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
 
     @Override
     public User create(User user) {
         List<User> users = findAll();
-        long max = 0L;
-        for (User u : users) {
-            long id = u.getId();
-            if (max < id){
-                max = id;
-            }
-        }
-        long next = max + 1;
+        long next = getMaxID(users) + 1;
         user.setId(next);
         users.add(user);
         List<String> lines = new ArrayList<>();
         for (User u: users) {
             lines.add(mapper.toInput(u));
         }
-        operation.saveAll(lines);
+        saveAll(lines);
         return user;
     }
-
+    private Long getMaxID (List<User> userList) {
+        long max = 0L;
+        for (User u : userList) {
+            long id = u.getId();
+            if (max < id){
+                max = id;
+            }
+        }
+        return max;
+    }
+    private void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     @Override
     public Optional<User> findById(Long id) {
+        List<User> users = findAll();
         return Optional.empty();
     }
 
